c194f4256be4a39a1341c9875fc27ca3ee9ccc70
DClaeyman <claeymand@gmail.com>
2019-05-22 21:53:12 +0200

    Updates

---
+++ precache-manifest.3a101ebd0a5e778fc397527632faf7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb71580f5adab1903963be813e4ebcc",
+    "url": "/Modular/index.html"
+  },
+  {
+    "revision": "d04450357ed8abee0ebd",
+    "url": "/Modular/static/css/2.f41ad63a.chunk.css"
+  },
+  {
+    "revision": "5e361963882b7e67a178",
+    "url": "/Modular/static/css/main.c8dc78d0.chunk.css"
+  },
+  {
+    "revision": "d04450357ed8abee0ebd",
+    "url": "/Modular/static/js/2.4d9112e8.chunk.js"
+  },
+  {
+    "revision": "5e361963882b7e67a178",
+    "url": "/Modular/static/js/main.f73330d7.chunk.js"
+  },
+  {
+    "revision": "f6e5c0cbfd2dcc719146",
+    "url": "/Modular/static/js/runtime~main.273ee721.js"
+  }
+]);
