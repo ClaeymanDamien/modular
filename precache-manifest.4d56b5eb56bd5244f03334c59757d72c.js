1850086c87311e4ad67cd26328e1fcd6ff04b9df
DClaeyman <claeymand@gmail.com>
2019-05-23 14:42:23 +0200

    Updates

---
+++ precache-manifest.4d56b5eb56bd5244f03334c59757d72c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6a2555cc6a55520b1daafcb42c8d18",
+    "url": "/modular/index.html"
+  },
+  {
+    "revision": "137699aa32ef0da3fb6c",
+    "url": "/modular/static/css/2.f41ad63a.chunk.css"
+  },
+  {
+    "revision": "f9243a07af7a6c32a730",
+    "url": "/modular/static/css/main.277faa1a.chunk.css"
+  },
+  {
+    "revision": "137699aa32ef0da3fb6c",
+    "url": "/modular/static/js/2.c997e712.chunk.js"
+  },
+  {
+    "revision": "f9243a07af7a6c32a730",
+    "url": "/modular/static/js/main.9beea93d.chunk.js"
+  },
+  {
+    "revision": "1cbc0f22addde22b92c6",
+    "url": "/modular/static/js/runtime~main.ada354d5.js"
+  },
+  {
+    "revision": "174af00d2a41260706da3a7201d15bb7",
+    "url": "/modular/static/media/food.174af00d.png"
+  },
+  {
+    "revision": "0b9ffb0c21063680e36c97b6a3699f3f",
+    "url": "/modular/static/media/logo.0b9ffb0c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modular/precache-manifest.ac2425eb5901cca191840e715ebd45d7.js"
+  "/modular/precache-manifest.4d56b5eb56bd5244f03334c59757d72c.js"
 );
 
 self.addEventListener('message', (event) => {
