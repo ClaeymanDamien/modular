88134635cfa650f94d270bdf77f90aacb9bb48f2
DClaeyman <claeymand@gmail.com>
2019-05-24 11:42:40 +0200

    Updates

--- precache-manifest.3645380cca2598d895b4dbe0806af4a0.js
+++ precache-manifest.3645380cca2598d895b4dbe0806af4a0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83df263dc4b94fccda9e53a466a1fc51",
+    "revision": "b00b262c43c7d0ce6c81924b19d9d1e7",
     "url": "/modular/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/modular/static/css/2.f41ad63a.chunk.css"
   },
   {
-    "revision": "e7bae8d85375dc092c3f",
-    "url": "/modular/static/css/main.d5c3b012.chunk.css"
+    "revision": "2121fc14ebf10a2ff3ce",
+    "url": "/modular/static/css/main.351dc39e.chunk.css"
   },
   {
     "revision": "56906a7cd6bd83bfc8fb",
     "url": "/modular/static/js/2.4b0177f7.chunk.js"
   },
   {
-    "revision": "e7bae8d85375dc092c3f",
-    "url": "/modular/static/js/main.4f551faa.chunk.js"
+    "revision": "2121fc14ebf10a2ff3ce",
+    "url": "/modular/static/js/main.af17d658.chunk.js"
   },
   {
     "revision": "1cbc0f22addde22b92c6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modular/precache-manifest.b781e54e2abb05c24f386d6a56a87bef.js"
+  "/modular/precache-manifest.3645380cca2598d895b4dbe0806af4a0.js"
 );
 
 self.addEventListener('message', (event) => {
